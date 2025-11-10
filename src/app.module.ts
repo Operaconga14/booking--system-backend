73d9c68904526504f3dc095c2484d2470b07bf8e
Amire Joseph <operacongahub@gmail.com>
2025-11-10 16:40:57 +0100

    Made changes to the Api config

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AuthModule } from './auth/auth.module';
 import { BookingModule } from './booking/booking.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AdminModule, AuthModule, BookingModule, TransactionModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    AdminModule, AuthModule, BookingModule, TransactionModule, UserModule],
   controllers: [],
   providers: [],
 })
