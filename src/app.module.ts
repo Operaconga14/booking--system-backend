a65399a63f7c50646f14ec9f0dd7586e4ada2659
Amire Joseph <operacongahub@gmail.com>
2025-11-10 16:45:50 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,25 @@ import { BookingModule } from './booking/booking.module';
 import { TransactionModule } from './transaction/transaction.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      synchronize: false,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      autoLoadEntities: true
+    })
+    ,
     AdminModule, AuthModule, BookingModule, TransactionModule, UserModule],
   controllers: [],
   providers: [],
