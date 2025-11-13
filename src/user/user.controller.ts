8e3c943d386132333fc6f1c72fd741f5fa0d6b54
Amire Joseph <operacongahub@gmail.com>
2025-11-13 17:44:41 +0100

    Update user.controller.ts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,12 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
 import { AuthGuard } from 'src/guards/auth.guard';
-import { Roles } from 'src/decorators/role.decorator';
-import { RoleGuard } from 'src/guards/role.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
-  @UseGuards(AuthGuard, RoleGuard)
-  @Roles('user')
+  @UseGuards(AuthGuard)
   @Get('profile')
   profile(@Req() req: any) {
     return this.userService.getProfile(req)
