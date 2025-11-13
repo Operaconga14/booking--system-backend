c3e308be3ab1c68d66c8ce9d20391efb9367fa65
Amire Joseph <operacongahub@gmail.com>
2025-11-13 17:24:22 +0100

    Added Auth Guard and get User detils

---
+++ src/guards/auth.guard.ts
@@ -0,0 +1,31 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { TokenUtilsService } from 'src/utils/token.utils.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private readonly tokenService: TokenUtilsService) { }
+
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['authorization'];
+
+    if (!authHeader || !authHeader.startsWith('Bearer '))
+      throw new UnauthorizedException('No token provided');
+
+    try {
+
+      const token = authHeader.split(' ')[1];
+      const user = this.tokenService.verifyToken(token);
+      request.user = user;
+
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
+
+  }
+}
