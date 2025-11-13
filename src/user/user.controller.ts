edc5e636447b6c74ce99acf499a2b4995cdefb11
Amire Joseph <operacongahub@gmail.com>
2025-11-13 17:47:40 +0100

    Removed auth guard

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, Req, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
-import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) { }
 
-  @UseGuards(AuthGuard)
   @Get('profile')
   profile(@Req() req: any) {
     return this.userService.getProfile(req)
